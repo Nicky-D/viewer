bbaf131d053c93dbe637d210f8917fd2fbf3e083
Nicky <nicky.dasmijn@posteo.nl>
2023-09-28 21:28:25 +0200

    Better SDL handling for ndof cmake, instead of coding the SDL version in two places, define it in LLWindow.cmake and use those settings in NDOF.cmake

--- indra/cmake/LLWindow.cmake
+++ indra/cmake/LLWindow.cmake
@@ -10,8 +10,16 @@ add_library( ll::SDL INTERFACE IMPORTED )
 
 if (LINUX)
   #Must come first as use_system_binary can exit this file early
-  target_compile_definitions( ll::SDL INTERFACE LL_SDL=1 LL_SDL_VERSION=3 )
-  set( SDL_VERSION SDL3 )
+
+  if( USE_SD2 )
+    target_compile_definitions( ll::SDL INTERFACE LL_SDL=1 LL_SDL_VERSION=2 )
+    set( SDL_VERSION SDL3 )
+    set( SDL_VERSION_LOWERCASE sdl3 )
+  else()
+    target_compile_definitions( ll::SDL INTERFACE LL_SDL=1 LL_SDL_VERSION=3 )
+    set( SDL_VERSION SDL3 )
+    set( SDL_VERSION_LOWERCASE sdl3 )
+  endif()
 
   use_system_binary(SDL)
   use_prebuilt_binary(SDL)
